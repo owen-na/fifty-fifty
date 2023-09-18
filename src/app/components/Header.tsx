e1561f8842763eeed38afc44d2bed41cf0ab20a2
owen <osn53387@uga.edu>
2023-09-18 12:51:26 -0400

    logo loaded in on header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,16 @@
 import styles from "@styles/Header.module.css"
+import Image from "next/image"
+import Logo from "../assets/Untitled.jpg"
 
 export default function Header() {
     return (
         <div className={styles.main}>
+            <Image 
+            src={Logo}
+            width={70}
+            height={70} /** these values are placeholders, real style for measurements are in the css */
+            className={styles.check}
+            alt="logo"/>
             <button>Casual</button>
             <button>Career</button>
             <button>Competitive</button>
